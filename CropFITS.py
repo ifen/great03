716fac4fb634e8779410bf42d9aaafe106611790
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-23 15:52:31 +0100

    Code Organisation

--- CropFITS.py
+++ CropFITS.py
@@ -4,20 +4,16 @@ import pyfits
 import matplotlib.pyplot as plt
 from pylab import *
 
+#LOAD IMAGES
 FitsHDU = pyfits.open('/home/ian/Documents/GREAT03/0/image-000-0.fits')
 ImOrig = FitsHDU[0].data
 FitsHeader = FitsHDU[0].header
 
-#Crop the image
+#EXTRACT THE DATA FROM THE FITS FILE
 Im = ImOrig[0:200, 4600:4800]
 FitsHDU[0].data = Im
 
-#Write it to a new file
-# FitsHDU.writeto(OutFile)
-# plt.imshow(Im, aspect='auto', origin='lower')
-# plt.imshow(ImOrig, aspect='auto', origin='lower')
-# plt.show()
-
+#PLOT CROPPED IMAGE AND ORIGINAL
 fig = plt.figure()
 ax = fig.add_subplot(121)
 ax.imshow(Im, aspect='auto', origin='lower')
