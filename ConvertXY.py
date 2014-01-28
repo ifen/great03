6be4d7b6680ce00c2d51db5720eec3e1448fc58e
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-28 10:48:18 +0100

    Updated files to load/save to the correct directories

--- ConvertXY.py
+++ ConvertXY.py
@@ -7,17 +7,17 @@ import pyfits
 
 # PERFORM THE FILE CONVERSION USING XYTOSKY MODULE
 sys.stdout = open(os.devnull, "w")
-output = xyConv.XYtoSky_pars('/home/ian/Documents/GREAT03/sample/g1.fits[0]', None, None, '/home/ian/Documents/GREAT03/sample/galaxy_catalog-000.fits', 'x,y', xyConv.yes, 'IDCTAB')
+output = xyConv.XYtoSky_pars('/home/ian/Documents/GREAT03/0/out/image-000-0X.fits[0]', None, None, '/home/ian/Documents/GREAT03/0/galaxy_catalog-000.fits', 'x,y', xyConv.yes, 'IDCTAB')
 sys.stdout = sys.__stdout__
 
 # LOAD AND SAVE THE G03 GALAXY IDS
-f2 = pyfits.open('/home/ian/Documents/GREAT03/sample/galaxy_catalog-000.fits')
+f2 = pyfits.open('/home/ian/Documents/GREAT03/0/galaxy_catalog-000.fits')
 tbdata = f2[1].data
 idData = []
 
 # COMBINE AND CLOSE THE IDS + NEW (RA, DECS) AND SAVE TO ASC FILE
-f = open('/home/ian/Documents/GREAT03/sample/g1.asc', 'w')
-f.write("RA\t\tDEC\n")
+f = open('/home/ian/Documents/GREAT03/0/out/image-000-0X.asc', 'w')
+f.write("RA\t\tDEC\t\tID\n")
 
 for (counter, tableData) in enumerate(tbdata.base):
     idData.append(tableData[2])
--- WriteHeadFile.py
+++ WriteHeadFile.py
@@ -3,9 +3,9 @@ __author__ = 'Ian Fenech Conti'
 import pyfits
 
 # LOAD AND SAVE THE G03 GALAXY IDS
-f = open('/home/ian/Documents/GREAT03/sample/g1.head', 'w')
+f = open('/home/ian/Documents/GREAT03/0/out/image-000-0X.head', 'w')
 
-hdulist = pyfits.open('/home/ian/Documents/GREAT03/sample/g1.fits')
+hdulist = pyfits.open('/home/ian/Documents/GREAT03/0/out/image-000-0X.fits')
 prihdr = hdulist[0].header
 
 for headerInfo in prihdr.cards:
