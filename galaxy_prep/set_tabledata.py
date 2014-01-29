62ee4e471b5f639ccd5763811d536b10315291a8
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-29 15:34:22 +0100

    align the galaxy coordiantes with the to a standard fits format

---
+++ galaxy_prep/set_tabledata.py
@@ -0,0 +1,14 @@
+__author__ = 'Ian Fenech Conti'
+
+import pyfits
+
+f = pyfits.open('/home/ian/Documents/GREAT03/0/galaxy_catalog-000.fits')
+
+table_data = f[1].data
+
+for (counter, tableData) in enumerate(table_data.base):
+    table_data[counter][0] = table_data[counter][0] + 1
+    table_data[counter][1] = table_data[counter][1] + 1
+
+hdu = pyfits.BinTableHDU(table_data)
+hdu.writeto('/home/ian/Documents/GREAT03/0/out/galaxy_catalog-000-X.fits')
