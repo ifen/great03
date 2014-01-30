bb63f1c1036c443dace0af63eb4bad20f5f4fb47
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-30 18:35:23 +0100

    Changes to Cropping Code/ Data Reading

--- star_prep/crop_psf.py
+++ star_prep/crop_psf.py
@@ -6,6 +6,11 @@ from pylab import *
 fits_hdu = pyfits.open('/home/ian/Documents/GREAT03/0/out/starfieldimage-000-0X.fits')
 fits_image = fits_hdu[0].data
 fits_crop = fits_image[0:48, 0:48]
+fits_hdu[0].data = fits_crop
+
+print len(fits_crop)
 
 plt.imshow(fits_crop, aspect='auto', origin='lower')
 plt.show()
+
+fits_hdu.writeto('/home/ian/Documents/GREAT03/0/out/starfield_crop-000-0X.fits')
