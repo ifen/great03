f5a67af4b998c18652f5bbd4ac19c3bb38c3e49f
Ian Fenech Conti <ianfc89@gmail.com>
2014-01-30 17:19:44 +0100

    Arranged Index Files. Increments to pixel positions to center galaxy on correct pixel

--- galaxy_prep/set_tabledata.py
+++ galaxy_prep/set_tabledata.py
@@ -7,7 +7,7 @@ f = pyfits.open('/home/ian/Documents/GREAT03/0/galaxy_catalog-000.fits')
 table_data = f[1].data
 
 for (counter, tableData) in enumerate(table_data.base):
-    table_data[counter][0] = table_data[counter][0] + 1
+    table_data[counter][0] = table_data[counter][0] + 2
     table_data[counter][1] = table_data[counter][1] + 1
 
 hdu = pyfits.BinTableHDU(table_data)
