a8e338b9560255a1778f74fb31c509422d25b061
Ian Fenech Conti <ianfc89@gmail.com>
2014-02-05 17:26:49 +0100

    Pipeline Formation : Changes to Flow

--- utils/main.py
+++ utils/main.py
@@ -6,14 +6,16 @@ from galaxy_prep.set_tabledata import *
 from galaxy_prep.write_headfile import *
 
 path_original = '/home/ian/Documents/GREAT03/0/image-000-0.fits'
-path_save = '/home/ian/Documents/GREAT03/0/out/image-000-0X.fits'
+path_save = '/home/ian/Documents/GREAT03/0/out/image0.fits'
+path_coeff = '/home/ian/Documents/GREAT03/0/out/image0.coeff.fits'
 path_dither = '/home/ian/Documents/GREAT03/0/epoch_dither-000-0.txt'
 path_offsets = '/home/ian/Documents/GREAT03/0/subfield_offset-000.txt'
-path_new = '/home/ian/Documents/GREAT03/0/out/image-000-0X.fits[0]'
+path_new = '/home/ian/Documents/GREAT03/0/out/image0.fits[0]'
 path_catalogue = '/home/ian/Documents/GREAT03/0/galaxy_catalog-000.fits'
-path_catalogue_deg = '/home/ian/Documents/GREAT03/0/out/galaxy_catalog-000-X.fits'
-path_asc = '/home/ian/Documents/GREAT03/0/out/image-000-0X.asc'
-path_headfile = '/home/ian/Documents/GREAT03/0/out/image-000-0X.head'
+path_catalogue_deg = '/home/ian/Documents/GREAT03/0/out/galaxy_catalog0.fits'
+path_asc = '/home/ian/Documents/GREAT03/0/out/image0.asc'
+path_headfile = '/home/ian/Documents/GREAT03/0/out/image0.head'
+
 
 prepare_header(path_offsets, path_dither,
                path_original, path_save)
@@ -30,4 +32,4 @@ convert(path_new,
 os.chdir("/home/ian/Documents/GREAT03/0/out/")
 os.system("./coeff "
           "/home/ian/Documents/GREAT03/0/starfield_crop-000-0X.fits "
-          "out.coeff.fits")
+          "%s" % path_coeff)
